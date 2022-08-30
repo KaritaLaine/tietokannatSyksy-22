a37cfc28fb5b8ddfaefd0d4c1a6d92fd2b6ae57a
KaritaLaine <44746@edu.raseko.fi>
2022-08-30 15:03:02 +0300

    luokka-määritykset ja kirjastojen lataus

---
+++ app.py
@@ -0,0 +1,12 @@
+# SOVELLUKSEN PÄÄOHJELMA
+
+# Kirjastojen ja modulien lataus
+import datetime
+
+# Luokka-määritykset
+
+class Person():
+    "docstring for ClassName."""
+    def __init__(self, firstName, lastName):
+        self.firstname = firstName
+        self.lastname = lastName
